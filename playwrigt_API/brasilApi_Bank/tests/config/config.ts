ad8dc0550b5d4cf8481fb57b4f663b27318ff955
Lima <cleibsonslima@gmail.com>
2025-12-01 09:08:56 -0300

    Beginning of work with API Brazil

---
+++ playwrigt_API/brasilApi_Bank/tests/config/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  baseURL: 'https://brasilapi.com.br/api',
+  endpoints: {
+    bank: 'banks/v1',
+    bank2: '',
+    usuarios: '',
+    produtos: '',
+    carrinhos: ''
+  }
+};
