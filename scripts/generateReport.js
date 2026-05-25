5837242d751f0bafc348e7ba745976ade646542a
Lima <cleibsonslima@gmail.com>
2026-05-25 18:54:22 -0300

    Final version with reports separated by date, including screenshots and Cucumber applied 2.

--- scripts/generateReport.js
+++ scripts/generateReport.js
@@ -1,7 +1,16 @@
 const { execSync } = require('child_process');
 
 const now = new Date();
-const timestamp = now.toISOString().replace(/[:.]/g, '-');
+
+const year = now.getFullYear();
+const month = String(now.getMonth() + 1).padStart(2, '0');
+const day = String(now.getDate()).padStart(2, '0');
+
+const hours = String(now.getHours()).padStart(2, '0');
+const minutes = String(now.getMinutes()).padStart(2, '0');
+const seconds = String(now.getSeconds()).padStart(2, '0');
+
+const timestamp = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
 
 const reportDir = `allure-reports/report-${timestamp}`;
 
